63fff6feb0bec186d40c304ae170ebf6dad9793e
dajana.milosevic@fazi.rs <dajana.milosevic@fazi.rs>
2024-08-14 01:31:22 +0200

    Add email form.

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -72,9 +72,6 @@
   }
 
   function displayError(thisForm, error) {
-    thisForm.querySelector('.loading').classList.remove('d-block');
-    thisForm.querySelector('.error-message').innerHTML = error;
-    thisForm.querySelector('.error-message').classList.add('d-block');
-  }
+     }
 
 })();
